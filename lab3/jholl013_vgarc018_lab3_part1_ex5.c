1f759d5e721ec7ef45461f790b141aff8c9e64de
Virgilio Garcia <vgarc018@users.noreply.github.com>
2015-04-23 08:20:35 -0700

    Update jholl013_vgarc018_lab3_part1_ex5.c

--- lab3/jholl013_vgarc018_lab3_part1_ex5.c
+++ lab3/jholl013_vgarc018_lab3_part1_ex5.c
@@ -2,7 +2,7 @@
  * James Hollister <jholl013@ucr.edu>
  * Virgilio Garcia <vgarc018@ucr.edu>
  * Lab Section: 23
- * Assignment: Lab #2 Part #1 Exercise #5
+ * Assignment: Lab #3 Part #1 Exercise #5
  *
  * I acknowledge all content contained herein, excluding template or example
  * code, is my own original work.
